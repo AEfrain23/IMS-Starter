e0e28b46667790ef84395297ae29edcba130c69e
Angel Puruncajas Guevara <angelefrain23@gmail.com>
2022-07-28 15:04:09 +0100

    Customer class tests all in working order

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -26,5 +26,16 @@
        PRIMARY KEY (`order_id`)
   ); 
   
+     CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
+       `order_items_id` INT(11) NOT NULL AUTO_INCREMENT,
+	   `order_id` INT(11) NOT NULL,
+	   `item_id` DOUBLE(10, 2) DEFAULT NULL,
+       `item_quantity` INT(11) NOT NULL,
+       PRIMARY KEY (`order_items_id`)
+       FOREIGN KEY (`order_id`)
+       FOREIGN KEY (`item_id`)
+       
+  ); 
+  
   
   
